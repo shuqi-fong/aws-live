2917aae434a679cb62b79eba89bcb207af321d78
shuqi-fong <101684961+shuqi-fong@users.noreply.github.com>
2023-08-29 16:32:48 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.ctllnrhwxttl.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "fongshuqi-employee"
+customregion = "us-east-1"
+
